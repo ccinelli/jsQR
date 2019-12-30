4acdfe6f6ac64dbfc7b781de641fb12d997dcef9
Chris Cinelli <ccinelli@ebay.com>
2019-12-30 12:55:30 -0800

    Report processing time in tests

--- tests/end-to-end-test.ts
+++ tests/end-to-end-test.ts
@@ -1,16 +1,21 @@
 import * as fs from "fs-extra";
 import * as path from "path";
 import jsQR from "../src";
-import { loadPng } from "../tests/helpers";
 import * as helpers from "./helpers";
 
 describe("end to end", async () => {
   const tests = fs.readdirSync(path.join("tests", "end-to-end")).filter((n) => !n.includes("."));
   for (const t of tests) {
+    // console.log("CREATING TEST ", t);
     it(t, async () => {
       const inputImage = await helpers.loadPng(path.join("tests", "end-to-end", t, "input.png"));
       const expectedOutput = JSON.parse(await fs.readFile(path.join("tests", "end-to-end", t, "output.json"), "utf8"));
-      expect(jsQR(inputImage.data, inputImage.width, inputImage.height)).toEqual(expectedOutput);
+
+      const start = +new Date();
+      const result = jsQR(inputImage.data, inputImage.width, inputImage.height);
+      console.log(`Test "${t}" took`, +new Date() - start, 'ms');
+
+      expect(result).toEqual(expectedOutput);
     });
   }
 });
